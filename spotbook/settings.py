03c3b9bf6137679724f70b56bf48e8ffde47cbff
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2022-09-21 14:10:29 +0100

    cors-headers requirements

--- spotbook/settings.py
+++ spotbook/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_gis',
 
+    'corsheaders',
     'leaflet',
 
     'spotbook.apps.accounts',
@@ -53,12 +54,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'spotbook.urls'
 
