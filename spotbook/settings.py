8abd6cfade1432f9e572136519f6bc380631b437
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2022-09-23 18:36:01 +0100

    auth api

--- spotbook/settings.py
+++ spotbook/settings.py
@@ -65,6 +65,9 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
