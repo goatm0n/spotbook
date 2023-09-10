e36a6b149ee191f8847a25fd1f1d880e0670187a
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2023-09-10 15:01:11 +0100

    whitenoise

--- spotbook/wsgi.py
+++ spotbook/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotbook.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+from whitenoise import WhiteNoise
+application = WhiteNoise(get_wsgi_application())
